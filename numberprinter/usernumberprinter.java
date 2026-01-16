c402607146221f08ecdb22bc0118928032d522f7
Umesh Ishwar Patil <uc2073593@gmail.com>
2026-01-16 23:30:39 +0530

    Add files via upload

---
+++ numberprinter/usernumberprinter.java
@@ -0,0 +1,17 @@
+package numberprinter;
+import java.util.Scanner;
+public class usernumberprinter {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("enter n :");
+		int n = sc.nextInt();
+		for(int i=1; i<=n;i++) {
+			for (int j=1; j<=n; j++) {
+				System.out.print(j  +" ");	
+		
+			}
+			System.out.println();
+		}
+	}
+
+}
